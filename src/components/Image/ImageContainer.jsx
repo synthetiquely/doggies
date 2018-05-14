d830842b9612f9e779d57c3979b6f7806ba02283
synthetiquely <anatoliybelobrovik@gmail.com>
2018-05-14 11:49:37 +0300

    Add hover effect

--- src/components/Image/ImageContainer.jsx
+++ src/components/Image/ImageContainer.jsx
@@ -8,6 +8,11 @@ export const Container = styled.div`
   align-items: center;
   background-color: ${props => (props.colored ? '#ccc' : 'f8f8f8')};
   cursor: pointer;
+  &:hover,
+  &:active,
+  &:focus {
+    filter: brightness(40%);
+  }
 `;
 
 export const ImageContainer = ({ errored, children }) => (
